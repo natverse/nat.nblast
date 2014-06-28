d87746e399fcfe062b4ab8d7560963aa794858c6
Gregory Jefferis <jefferis@gmail.com>
2014-06-28 23:06:50 +0100

    Document package options

--- R/nat.nblast.r
+++ R/nat.nblast.r
@@ -1,14 +1,23 @@
 #' Neuron similarity, search and clustering tools
 #'
-#' The main entry point for similarity and search functions is
-#' \code{\link{nblast}}. The current version (2) depends on a scoring matrix.
-#' Default matrices trained using Drosophila neurons are distributed with this
-#' package (see \code{\link{smat.fcwb}})
+#' @section Search:
+#'
+#'   The main entry point for similarity and search functions is
+#'   \code{\link{nblast}}. The current version (2) depends on a scoring matrix.
+#'   Default matrices trained using Drosophila neurons are distributed with this
+#'   package (see \code{\link{smat.fcwb}})
+#'
+#' @section Package Options:
+#'
+#'   There is one package option \code{nat.nblast.defaultsmat} which is set to
+#'   \code{'smat.fcwb'} when the package is loaded if it has not already been
+#'   set by someone else.
 #'
 #' @references Costa, M., Ostrovsky, A.D., Manton, J.D., Prohaska, S., and
 #'   Jefferis, G.S.X.E. (2014). NBLAST: Rapid, sensitive comparison of neuronal
 #'   structure and construction of neuron family databases. Biorxiv preprint.
 #'   \href{http://dx.doi.org/10.1101/006346}{doi: 10.1101/006346}.
+#'
 #' @name nat.nblast-package
 #' @aliases nat.nblast
 #' @docType package
