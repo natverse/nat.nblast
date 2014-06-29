414b2865f7c9d9ead88f8e78ad993c1bfd5c1b46
Gregory Jefferis <jefferis@gmail.com>
2014-06-29 20:42:14 +0100

    test for nblast v1 normalised score as well

--- tests/testthat/test-NBLAST1.r
+++ tests/testthat/test-NBLAST1.r
@@ -19,3 +19,9 @@ test_that("nblast v1 handles a neuronlist as query", {
   scores.expected <- structure(c(0, 0.999980027716081, 0.999848861700461, 0.979269468123817, 0.999876876458676, 0.999947995458016, 0, 0.999999998533649, 0.999980102828588, 0.99999999937142), .Dim = c(5L, 2L), .Dimnames = list(c("5HT1bMARCM-F000001_seg001", "5HT1bMARCM-F000002_seg001", "5HT1bMARCM-F000003_seg001", "5HT1bMARCM-F000004_seg001", "5HT1bMARCM-F000005_seg001"), c("5HT1bMARCM-F000001_seg001", "5HT1bMARCM-F000002_seg001")))
   expect_equal(scores, scores.expected)
 })
+
+test_that("we can calculate normalised nblast v1 scores", {
+  scores <- nblast(testneurons[1:2], testneurons, version=1)
+  scores.norm=scale(scores, scale=c(scores[1,1],scores[2,2]), center = FALSE)
+  expect_equivalent(nblast(testneurons[1:2], testneurons, version=1, normalised=TRUE), scores.norm)
+})
