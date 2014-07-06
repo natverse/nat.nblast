4f6315687a8aadc72a94829f2b31fe3599c112e5
Gregory Jefferis <jefferis@gmail.com>
2014-07-07 00:17:06 +0100

    Add full example for plot3d.hclust

--- R/clustering.r
+++ R/clustering.r
@@ -54,6 +54,15 @@ nhclust <- function(neuron_names, method='ward', scoremat=NULL, distfun=as.dist,
 #' @seealso
 #' \code{\link{nhclust}, \link[rgl]{plot3d}, \link{slice}, \link{colour_clusters}}
 #' @importFrom dendroextras slice
+#' @examples
+#' # 20 Kenyon cells
+#' data(kcs20, package='nat')
+#' # calculate mean, normalised NBLAST scores
+#' x=nblast(kcs20, kcs20, normalised=TRUE)
+#' x=(x+t(x))/2
+#' # note that specifying db explicitly could be avoided by use of the
+#' # \code{nat.default.neuronlist} option.
+#' plot3d(hclust(as.dist(x)), k=3, db=kcs20)
 plot3d.hclust <- function(x, k=NULL, h=NULL, groups=NULL, col=rainbow, ...) {
   # Cut the dendrogram into k groups of neurons. Note that these will now have
   # the neurons in dendrogram order
