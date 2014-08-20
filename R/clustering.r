1f9d79ec474711d3bf3e3da7763418e5254f4370
Gregory Jefferis <jefferis@gmail.com>
2014-08-20 23:43:12 +0100

    Complete example for nhclust
    * actually run the example
    * fixes some errors

--- R/clustering.r
+++ R/clustering.r
@@ -17,18 +17,21 @@
 #' @family scoremats
 #' @seealso \code{\link{hclust}, \link{dist}}
 #' @examples
-#' \dontrun{
-#' data(kcs20, package='nat')
-#' hckcs=nhclust(names(kcs20))
-#' # dividide hclust object into 3 groups
-#' library(dendroextras)
-#' plot(colour_clusters(hckcs, k=3))
-#' # 3d plot of neurons in those clusters (with matching colours)
 #' library(nat)
+#' kcscores=nblast_allbyall(kcs20)
+#' hckcs=nhclust(scoremat=kcscores)
+#' # divide hclust object into 3 groups
+#' library(dendroextras)
+#' dkcs=colour_clusters(hckcs, k=3)
+#' # change dendrogram labels to neuron type, extracting this information
+#' # from type column in the metadata data.frame attached to kcs20 neuronlist
+#' labels(dkcs)=with(kcs20[labels(dkcs)], type)
+#' plot(dkcs)
+#' # 3d plot of neurons in those clusters (with matching colours)
+#' open3d()
 #' plot3d(hckcs, k=3, db=kcs20)
 #' # names of neurons in 3 groups
 #' subset(hckcs, k=3)
-#' }
 nhclust <- function(neuron_names, method='ward', scoremat=NULL, distfun=as.dist, ..., maxneurons=4000) {
   subdistmat <- sub_dist_mat(neuron_names, scoremat, maxneurons=maxneurons)
   if(min(subdistmat) < 0)
