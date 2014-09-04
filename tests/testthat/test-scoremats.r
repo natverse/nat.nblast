49810a674d2ce5564dd3bc361167a294c15b138d
James Manton <ajd.manton@googlemail.com>
2014-09-04 15:44:41 +0100

    Fix test of sparse matrices to deal with fact that diagonal is now inc.

--- tests/testthat/test-scoremats.r
+++ tests/testthat/test-scoremats.r
@@ -6,5 +6,5 @@ test_that("Conversion to sparse matrix representation is correct", {
   dense_smat <- nblast(testneurons, testneurons)
   sparse_smat <- sparse_score_mat(names(testneurons)[2:4], dense_smat)
   expect_true(all(sparse_smat[c(2, 3), c(3, 4)] == dense_smat[c(2, 3), c(3, 4)]))
-  expect_equal(0, sparse_smat[1, 1])
+  expect_equal(0, sparse_smat[1, 5])
 })
