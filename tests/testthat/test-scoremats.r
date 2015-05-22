2058d56a6af295858970177dcdbe2f93524c8d2c
Gregory Jefferis <jefferis@gmail.com>
2015-05-22 21:36:51 +0100

    test sub_score_mat with non square inputs/outputs

--- tests/testthat/test-scoremats.r
+++ tests/testthat/test-scoremats.r
@@ -1,8 +1,27 @@
-context("Sparse score matrix functions")
+context("Score matrix functions")
 
 testneurons <- readRDS('testdata/testneurons.rds')
 
 dense_smat <- nblast(testneurons, testneurons)
+
+test_that("can generate non square score matrix from square score matrix", {
+  nn=colnames(dense_smat)
+
+  expect_equal(sub_score_mat(nn, nn, scoremat = dense_smat), dense_smat)
+  expect_equal(q234t12<-sub_score_mat(query = nn[2:4], target = nn[1:2],
+                             scoremat = dense_smat),
+               dense_smat[nn[1:2],nn[2:4]])
+  expect_equal(q231t21<-sub_score_mat(query = nn[c(2,3,1)], target = nn[2:1],
+                             scoremat = dense_smat),
+               dense_smat[nn[2:1],nn[c(2,3,1)]])
+
+  expect_equal(sub_score_mat(query = nn[c(2,3,4)], target = nn[2:1],scoremat=q234t12),
+               dense_smat[nn[2:1],nn[c(2,3,4)]])
+})
+
+
+context("Sparse score matrix functions")
+
 sparse_smat <- sparse_score_mat(names(testneurons)[2:4], dense_smat)
 
 test_that("conversion to sparse matrix representation is correct", {
